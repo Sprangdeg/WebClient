c848aaca5676a4b3b437a9ac6ac5a57b23171eda
sprangdeg <d.bonstrom@gmail.com>
2016-10-15 15:09:14 +0200

    Vet ej vad det finns här

--- src/app/stryktipset.service.ts
+++ src/app/stryktipset.service.ts
@@ -13,8 +13,8 @@ import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class StryktipsetService {
-  private stryktipsetUrl = 'http://localhost/webapi/api/stryktipset';  // URL to web API
-  //private stryktipsetUrl = 'http://localhost:2021/api/Stryktipset';  // URL to web API
+  //private stryktipsetUrl = 'http://traitors.northeurope.cloudapp.azure.com/webapi/api/stryktipset';  // URL to web API
+  private stryktipsetUrl = 'http://localhost:2021/api/Stryktipset';  // URL to web API
   constructor (private http: Http) {}
 
   getStryktipsetCoupong () : Observable<Match[]> {
@@ -61,9 +61,9 @@ export class StryktipsetService {
 
    for (let mat of arr) {
       marks.push(<MarkRow>({
-        HomeMark: mat.AwayMark,
+        HomeMark: mat.HomeMark,
         DrawMark: mat.DrawMark,
-        AwayMark: mat.HomeMark,
+        AwayMark: mat.AwayMark,
         }))
     }
     return marks;
